bc1fddc07965871b54ffb3c105d2e7ae77caffc3
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-15 01:53:28 +0300

    add repository for users

---
+++ src/main/java/hexlet/code/demo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package hexlet.code.demo.repository;
+
+import hexlet.code.demo.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
