8d091d061c80368b1a3f57f3b180e5d293069dc8
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-15 01:54:00 +0300

    create security and encoders configs

---
+++ src/main/java/hexlet/code/demo/config/DataInitializer.java
@@ -0,0 +1,32 @@
+package hexlet.code.demo.config;
+
+import hexlet.code.demo.model.User;
+import hexlet.code.demo.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (userRepository.findByEmail("hexlet@example.com") == null) {
+            var user = new User();
+            user.setEmail("hexlet@example.com");
+            user.setPassword(passwordEncoder.encode("qwerty"));
+            userRepository.save(user);
+        }
+    }
+}
