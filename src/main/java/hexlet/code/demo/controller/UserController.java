ef78b40399e9f84dfcf0073ac9c055ff693a82df
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-15 01:54:34 +0300

    add rest controller for User

---
+++ src/main/java/hexlet/code/demo/controller/UserController.java
@@ -0,0 +1,61 @@
+package hexlet.code.demo.controller;
+
+import hexlet.code.demo.dto.UserCreateDTO;
+import hexlet.code.demo.dto.UserDTO;
+import hexlet.code.demo.dto.UserUpdateDTO;
+
+import hexlet.code.demo.service.UserService;
+
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO show(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDTO> index() {
+        return userService.getUsersAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO create(@Valid @RequestBody UserCreateDTO dto) {
+        return userService.createUser(dto);
+    }
+
+    @PutMapping("/{id}")
+    public UserDTO update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+        return userService.updateUser(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
