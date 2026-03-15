47c427cdd0e3a839eef749b233c63a74308131df
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-15 15:04:00 +0300

    add DTO for auth request

---
+++ src/main/java/hexlet/code/demo/dto/AuthRequestDTO.java
@@ -0,0 +1,13 @@
+package hexlet.code.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequestDTO {
+
+    private String username;
+
+    private String password;
+}
