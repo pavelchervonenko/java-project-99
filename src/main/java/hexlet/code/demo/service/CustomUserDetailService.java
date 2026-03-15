89ac1608e6cc267d289b41d3742fe0157ffc1267
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-15 15:05:19 +0300

    create bridge between security config and database

---
+++ src/main/java/hexlet/code/demo/service/CustomUserDetailService.java
@@ -0,0 +1,23 @@
+package hexlet.code.demo.service;
+
+import hexlet.code.demo.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
+    }
+}
