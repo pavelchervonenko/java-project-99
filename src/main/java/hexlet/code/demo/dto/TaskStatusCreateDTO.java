d4409e8515025bcd822b8d40f9e75bed6af94e2c
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-17 23:52:29 +0300

    create DTO for model TaskStatus

---
+++ src/main/java/hexlet/code/demo/dto/TaskStatusCreateDTO.java
@@ -0,0 +1,19 @@
+package hexlet.code.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskStatusCreateDTO {
+
+    @NotBlank
+    @Size(min = 1)
+    private String name;
+
+    @NotBlank
+    @Size(min = 1)
+    private String slug;
+}
---
+++ src/main/java/hexlet/code/demo/dto/TaskStatusUpdateDTO.java
@@ -0,0 +1,15 @@
+package hexlet.code.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskStatusUpdateDTO {
+
+    @NotBlank
+    @Size(min = 1)
+    private String name;
+}
