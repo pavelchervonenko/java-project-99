efe26b834b9c419e365e65fb2b79c8949361f6dd
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-19 01:17:42 +0300

    create DTOs for Labels

---
+++ src/main/java/hexlet/code/demo/dto/LabelUpdateDTO.java
@@ -0,0 +1,15 @@
+package hexlet.code.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LabelUpdateDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 1000)
+    private String name;
+}
