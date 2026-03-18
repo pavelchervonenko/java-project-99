c253865217043fd4077300bc6f20a8c6ce70ed8c
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-18 20:12:58 +0300

    create mapper for Task

---
+++ src/main/java/hexlet/code/demo/mapper/TaskMapper.java
@@ -0,0 +1,34 @@
+package hexlet.code.demo.mapper;
+
+import hexlet.code.demo.dto.TaskCreateDTO;
+import hexlet.code.demo.dto.TaskDTO;
+import hexlet.code.demo.dto.TaskUpdateDTO;
+
+import hexlet.code.demo.model.Task;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring",
+        uses = {ReferenceMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface TaskMapper {
+
+    @Mapping(target = "assignee", source = "assigneeId")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "toTaskStatus")
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    Task toEntity(TaskCreateDTO dto);
+
+    @Mapping(target = "assigneeId", source = "assignee.id")
+    @Mapping(target = "status", source = "taskStatus", qualifiedByName = "toSlug")
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "content", source = "description")
+    TaskDTO toDTO(Task model);
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    void updateEntityFromDTO(TaskUpdateDTO dto, @MappingTarget Task model);
+}
