0a0980e02463a170244f42573e088c69cf5bfb9a
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-18 20:14:25 +0300

    create new repo for new model

---
+++ src/main/java/hexlet/code/demo/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package hexlet.code.demo.repository;
+
+import hexlet.code.demo.model.Task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
--- src/main/java/hexlet/code/demo/repository/TaskStatusRepository.java
+++ src/main/java/hexlet/code/demo/repository/TaskStatusRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaskStatusRepository extends JpaRepository<TaskStatus, Long> {
-    TaskStatus findBySlug(String slug);
+    Optional<TaskStatus> findBySlug(String slug);
 }
