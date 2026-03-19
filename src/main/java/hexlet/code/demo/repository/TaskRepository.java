2c657b38ae157fbfeb814f19608d157def1b6473
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-19 10:55:56 +0300

    updated extends

--- src/main/java/hexlet/code/demo/repository/TaskRepository.java
+++ src/main/java/hexlet/code/demo/repository/TaskRepository.java
@@ -3,9 +3,10 @@ package hexlet.code.demo.repository;
 import hexlet.code.demo.model.Task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 }
