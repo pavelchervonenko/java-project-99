29dc016b5d50947a89b22264de2e385bd3bab29c
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-03-20 14:26:06 +0300

    remove @Size for users's passwrod for hexlet-check

--- src/main/java/hexlet/code/demo/dto/UserCreateDTO.java
+++ src/main/java/hexlet/code/demo/dto/UserCreateDTO.java
@@ -2,7 +2,6 @@ package hexlet.code.demo.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +19,5 @@ public class UserCreateDTO {
     private String email;
 
     @NotBlank
-    @Size(min = 6)
     private String password;
 }
